b8c4840f41107d844e6c6b6ba8155372e4fd2f9d
YOONSU PARK <shinebytheriver@yahoo.com>
2021-09-03 08:04:42 -0700

    Add files via upload
    2

---
+++ build/precache-manifest.31dd5cb636d54ad1711808fb8f84e205.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "794cb255bb3e82bb04cb",
+    "url": "/static/js/main.794cb255.chunk.js"
+  },
+  {
+    "revision": "ef57071cd3f90a388c08",
+    "url": "/static/js/1.ef57071c.chunk.js"
+  },
+  {
+    "revision": "ef57071cd3f90a388c08",
+    "url": "/static/css/1.c2ea04b5.chunk.css"
+  },
+  {
+    "revision": "ba337bcbba80e349e9ab0e8628cb6751",
+    "url": "/index.html"
+  }
+];
